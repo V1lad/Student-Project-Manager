01a133b53fc66c2a306baef2f1fb3d0b058d2479
Vlad <test@mawseasease.qwe>
2023-12-12 09:30:49 +0300

    Adjusted views

--- Web/views.py
+++ Web/views.py
@@ -2,19 +2,15 @@ from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 from . import db
 
-
 views = Blueprint('views', __name__)
-
 
 # This is main page of an application.
 @views.route('/')
 def main_():
-
     return render_template("main.html", user=current_user)
 
 # This is user's personal page.
 @views.route('/home')
 @login_required
 def home():
-
     return render_template("home.html", user=current_user, db=db)
