70486d5cc98b7b1d679a2c02ce2ec19867a7f52f
Vlad <test@mawseasease.qwe>
2023-12-13 21:10:16 +0300

    Finished redact subproject
    123

--- Web/projects.py
+++ Web/projects.py
@@ -101,18 +101,15 @@ def newProject():
         
         return redirect(url_for('projects.allProjects'))
 
-@projects.route('/projects/<int:index>', methods=["GET", "POST"])
+@projects.route('/projects/<int:index>', methods=["POST"])
 @login_required
 def showProject(index):
     # Надо сделать проверку на доступ к проекту.
     if request.method == "POST":
         to_create_subproject_name = request.form.get('to_create_subproject_name')
-        to_delete_subproject_id = request.form.get('to_delete_subproject_id')
-        to_redact_subproject_id = request.form.get('to_redact_subproject_id')
-        to_delete_word = request.form.get('to_delete_word')
+        
         
         print(to_create_subproject_name, "aaaaaaaaaaaaaa")
-        print(to_delete_subproject_id, "delete")
         project = Project.query.filter_by(id=index).first()
         subprojects = project.subprojects
         
@@ -123,127 +120,42 @@ def showProject(index):
             subproject = SubProject(name = to_create_subproject_name, parent_id = project.id)
             db.session.add(subproject)
             subprojects.append(subproject)
-        elif to_delete_subproject_id:
-            subproject = SubProject.query.filter_by(id=to_delete_subproject_id).first()
-            if subproject:
-                db.session.delete(subproject)
-                subprojects.remove(subproject)
-                
-        elif to_redact_subproject_id:
-            pass
+
         
         db.session.commit()
         return render_template("show_project.html", project=project, user=current_user, subprojects=subprojects)
 
-'''
-@testing.route('/testing', methods=["POST", "GET"])
+@projects.route('/projects/<int:index>/<int:subproject>/redact', methods=["POST"])
 @login_required
-def test():
-    if current_user.roles[0].name == 'student' and current_user.status == "active":
+def redactSubProject(index, subproject):
+    if request.method == "POST":
+        name = request.form.get('name')
+        description = request.form.get('description')
+        delete_word = request.form.get('delete')
+        
+        project = Project.query.filter_by(id=index).first()
+    
+        
+        subproject = SubProject.query.filter_by(id=subproject).first()
+        
+        if delete_word == "УДАЛИТЬ":
+            db.session.delete(subproject)
+            db.session.commit()
+            return redirect(url_for('projects.allProjects'))
+        
+        if name:
+            subproject.name = name
+        elif description:
+            subproject.shortDescription = description
 
-        tasks_to_delete = check_tasks()
-
-        if request.method == 'POST':
-            if request.form.get("task"):
-                task = request.form.get("task")
-                task = Task.query.get(int(task))
-
-                return render_template("active_testing.html", user=current_user, task=task)
-            else:
-                data = request.form
-                task_id = int(request.form.get("task_id"))
-                questions_ans = {}
-                for question in data:
-                    if question != "task_id":
-                        questions_ans[Question.query.get(int(question))] = data[question]
-
-                questions_results = {}
-
-                correct_answers = 0
-                incorrect_answers = 0
-
-                for question in questions_ans:
-                    if question.check(questions_ans[question]):
-                        questions_results[question.id] = True
-                        correct_answers += 1
-                    else:
-                        questions_results[question.id] = False
-                        incorrect_answers += 1
-
-                if incorrect_answers:
-                    percentage=(correct_answers/(incorrect_answers + correct_answers)) * 100
-                else:
-                    percentage = 100
-
-                if percentage > 85:
-                    mark = 5
-                elif 70 < percentage < 86:
-                    mark = 4
-                elif 50 < percentage:
-                    mark = 3
-                else:
-                    mark = 2
-
-                flag = True
-                for res in current_user.results:
-                    if res.test_id == task_id:
-                        flag = False
-
-                if flag:
-                    new_result = Result(test_id=task_id, student_id=current_user.id, content=code(questions_results), percentage=percentage, mark=mark, task_name=Task.query.get(task_id).name)
-
-                    db.session.add(new_result)
-                    db.session.commit()
-
-                return redirect(url_for("testing.results"))
-
-        params = []
-        for i in current_user.results:
-            params.append(i.test_id)
-
-        # for task in Task.query.all():
-        #     if not task.questions and (task.status == "active" or task.status == "planned" or task.status == "inactive"):
-        #         db.session.delete(task)
-        #         db.session.commit()
-
-        return render_template("testing.html", user=current_user, params=params)
-    else:
-        return "Для вашего типа аккаунта данная страница не доступна"
-
-
-@testing.route('/testing_results', methods=["POST", "GET"])
+        db.session.commit()    
+        return render_template("redact_subproject.html", project=project, user=current_user, subproject=subproject)
+    
+@projects.route('/projects/<int:index>/<int:subproject>', methods=["POST"])
 @login_required
-def results():
-    if current_user.roles[0].name == 'student' and current_user.status == "active":
-        return render_template("testing_results.html", user=current_user, Question=Question, Task=Task, decode=decode)
-    else:
-        return "Для вашего типа аккаунта данная страница не доступна"
-
-
-def check_tasks():
-    tasks_to_delete = []
-    for task in Task.query.all():
-        if task.status == "planned":
-            if get_date(task.beginning_time) < datetime.now():
-                task.status = "active"
-        elif task.status == "active":
-            if get_date(task.ending_time) < datetime.now():
-                tasks_to_delete.append(task)
-                task.status = "inactive"
-                for group in task.groups:
-                    for student in group.members:
-                        flag = False
-                        for result in student.results:
-                            if result.test_id == task.id:
-                                flag = True
-                        if not flag:
-                            contents = {}
-                            for question in task.questions:
-                                contents[question.id] = 0
-                            new_result = Result(test_id=task.id, student_id=student.id, content=code(contents), percentage=0, mark=2, task_name=task.name)
-                            db.session.add(new_result)
-        db.session.commit()
-
-
-    return tasks_to_delete
-'''
+def redactSubProject(index, subproject):
+    if request.method == "POST":
+        project = Project.query.filter_by(id=index).first()
+        subproject = SubProject.query.filter_by(id=subproject).first()
+        
+        return render_template("show_subproject.html", project=project, user=current_user, subproject=subproject)
